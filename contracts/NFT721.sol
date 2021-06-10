7ee7ffe4a9252899da141d44b84a37ceb4930b21
Team LevX <team@levx.io>
2021-06-10 16:31:00 +0900

    Fix NFT721

--- contracts/NFT721.sol
+++ contracts/NFT721.sol
@@ -83,7 +83,6 @@ contract NFT721 is ERC721Initializable, OwnableInitializable, ProxyFactory, Tagg
 
     function closeSale(uint256 tokenId) public override onlyOwnerOf(tokenId) {
         address sale = openSaleOf[tokenId];
-        require(sale == msg.sender, "SHOYU: FORBIDDEN");
         IStrategy(sale).cancel();
         openSaleOf[tokenId] = address(0);
 
