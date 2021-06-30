4d3600574cf47b5ebb68f3c189a0429af7fcd0c6
Team LevX <team@levx.io>
2021-06-30 16:33:53 +0900

    Let SocialToken extend AccessControl not Ownable

--- contracts/SocialToken.sol
+++ contracts/SocialToken.sol
@@ -2,13 +2,15 @@
 
 pragma solidity =0.8.3;
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./base/DividendPayingERC20.sol";
-import "./base/OwnableInitializable.sol";
 
-contract SocialToken is DividendPayingERC20, OwnableInitializable {
+contract SocialToken is DividendPayingERC20, AccessControl {
     event Mint(address indexed account, uint256 indexed value);
     event Burn(address indexed account, uint256 indexed value, bytes32 data);
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     function initialize(
         string memory _name,
@@ -17,16 +19,21 @@ contract SocialToken is DividendPayingERC20, OwnableInitializable {
         address _owner
     ) external initializer {
         __DividendPayingERC20_init(_name, _symbol, _dividendToken);
-        __Ownable_init(_owner);
+        _setupRole(DEFAULT_ADMIN_ROLE, _owner);
+        _setupRole(MINTER_ROLE, _owner);
     }
 
-    function mint(address account, uint256 value) external onlyOwner {
+    function mint(address account, uint256 value) external {
+        require(hasRole(MINTER_ROLE, msg.sender), "SHOYU: FORBIDDEN");
+
         _mint(account, value);
+
         emit Mint(account, value);
     }
 
     function burn(uint256 value, bytes32 data) external {
         _burn(msg.sender, value);
+
         emit Burn(msg.sender, value, data);
     }
 }
