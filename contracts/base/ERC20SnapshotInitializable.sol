685825bef4f47f5fdcad5832dca96a953965f784
Team LevX <team@levx.io>
2021-07-12 02:25:13 +0900

    Fix

--- contracts/base/ERC20SnapshotInitializable.sol
+++ contracts/base/ERC20SnapshotInitializable.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity =0.8.3;
 
 import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/utils/Arrays.sol";
