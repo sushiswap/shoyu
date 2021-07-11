52fdb5db39546806f074c6eb4f1f6538415db17c
Team LevX <team@levx.io>
2021-07-12 02:27:25 +0900

    Remove unneccessary code

--- contracts/libraries/TokenHelper.sol
+++ contracts/libraries/TokenHelper.sol
@@ -29,25 +29,4 @@ library TokenHelper {
             IERC20(token).safeTransfer(to, amount);
         }
     }
-
-    function safeTransferFromSender(address token, uint256 amount) internal {
-        if (token == ETH) {
-            require(msg.value == amount, "SHOYU: INVALID_MSG_VALUE");
-        } else {
-            IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
-        }
-    }
-
-    function safeTransferFromSender(
-        address token,
-        address to,
-        uint256 amount
-    ) internal {
-        if (token == ETH) {
-            require(msg.value == amount, "SHOYU: INVALID_MSG_VALUE");
-            payable(to).transfer(amount);
-        } else {
-            IERC20(token).safeTransferFrom(msg.sender, to, amount);
-        }
-    }
 }
