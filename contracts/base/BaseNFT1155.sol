ddf8d5bedcab3b7253cfe9d86c629e5a367cd8f1
Team LevX <team@levx.io>
2021-07-14 19:19:44 +0900

    Update BaseNFT1155.DOMAIN_SEPARATOR()

--- contracts/base/BaseNFT1155.sol
+++ contracts/base/BaseNFT1155.sol
@@ -35,7 +35,7 @@ abstract contract BaseNFT1155 is ERC1155Initializable, OwnableInitializable, IBa
         _DOMAIN_SEPARATOR = keccak256(
             abi.encode(
                 keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-                keccak256(abi.encodePacked(address(this))),
+                keccak256(bytes(Strings.toHexString(uint160(address(this))))),
                 keccak256(bytes("1")),
                 chainId,
                 address(this)
