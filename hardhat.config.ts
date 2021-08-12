7e071ee5d38237e453eaa376a329b2fe4b124cc2
Team LevX <team@levx.io>
2021-08-13 07:24:56 +0900

    v1.0.0-alpha.0

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-solhint";
 import "@nomiclabs/hardhat-waffle";
+import "hardhat-abi-exporter";
 import "hardhat-deploy";
 import "hardhat-gas-reporter";
 import "hardhat-spdx-license-identifier";
@@ -31,6 +32,13 @@ task("accounts", "Prints the list of accounts", async (args, { ethers }) => {
 });
 
 const config: HardhatUserConfig = {
+  abiExporter: {
+    path: "./abi",
+    clear: true,
+    flat: true,
+    only: [],
+    spacing: 2,
+  },
   defaultNetwork: "hardhat",
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
