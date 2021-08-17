c5048e059bcec46c740f4c1e30e2d48883a7f52a
TheGreatHB <kavanaboy@gmail.com>
2021-08-17 20:58:13 +0900

    Create TokenFactory.test.ts

---
+++ test/TokenFactory.test.ts
@@ -0,0 +1,79 @@
+import {
+    TokenFactory,
+    ERC721ExchangeV0,
+    ERC1155ExchangeV0,
+    NFT721V0,
+    NFT1155V0,
+    SocialTokenV0,
+    ERC721Mock,
+    ERC1155Mock,
+    ERC20Mock,
+} from "../typechain";
+
+import { ecsign } from "ethereumjs-util";
+import { hexlify } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { expect } from "chai";
+
+const { BigNumber, utils, constants } = ethers;
+const { AddressZero } = constants;
+
+const setupTest = async () => {
+    const signers = await ethers.getSigners();
+    const [deployer, protocolVault, operationalVault, alice, bob, carol] = signers;
+
+    const TokenFactoryContract = await ethers.getContractFactory("TokenFactory");
+    const factory = (await TokenFactoryContract.deploy(
+        protocolVault.address,
+        25,
+        operationalVault.address,
+        5,
+        "https://nft721.sushi.com/",
+        "https://nft1155.sushi.com/"
+    )) as TokenFactory;
+
+    const ERC721ExchangeContract = await ethers.getContractFactory("ERC721ExchangeV0");
+    const erc721Exchange = ERC721ExchangeContract.attach(await factory.erc721Exchange()) as ERC721ExchangeV0;
+
+    const ERC1155ExchangeContract = await ethers.getContractFactory("ERC1155ExchangeV0");
+    const erc1155Exchange = ERC1155ExchangeContract.attach(await factory.erc1155Exchange()) as ERC1155ExchangeV0;
+
+    const ERC721MockContract = await ethers.getContractFactory("ERC721Mock");
+    const erc721Mock = (await ERC721MockContract.deploy()) as ERC721Mock;
+
+    const ERC1155MockContract = await ethers.getContractFactory("ERC1155Mock");
+    const erc1155Mock = (await ERC1155MockContract.deploy()) as ERC1155Mock;
+
+    const ERC20MockContract = await ethers.getContractFactory("ERC20Mock");
+    const erc20Mock = (await ERC20MockContract.deploy()) as ERC20Mock;
+
+    return {
+        deployer,
+        protocolVault,
+        operationalVault,
+        factory,
+        erc721Exchange,
+        erc1155Exchange,
+        alice,
+        bob,
+        carol,
+        erc721Mock,
+        erc1155Mock,
+        erc20Mock,
+    };
+};
+
+describe("TokenFactory", () => {
+    beforeEach(async () => {
+        await ethers.provider.send("hardhat_reset", []);
+    });
+
+    it("should be FeeInfo functions return proper value", async () => {
+        const { protocolVault, operationalVault, factory } = await setupTest();
+
+        expect((await factory.protocolFeeInfo())[0]).to.be.equal(protocolVault.address);
+        expect((await factory.protocolFeeInfo())[1]).to.be.equal(25);
+        expect((await factory.operationalFeeInfo())[0]).to.be.equal(operationalVault.address);
+        expect((await factory.operationalFeeInfo())[1]).to.be.equal(5);
+    });
+});
