abaf1c7c21b64998cdb6d11f3a8e0cd1b324961e
TheGreatHB <kavanaboy@gmail.com>
2021-08-27 00:12:34 +0900

    Create blocks.ts

---
+++ test/utils/blocks.ts
@@ -0,0 +1,13 @@
+import { ethers } from "hardhat";
+import { expect } from "chai";
+
+export const getBlock = async (): Promise<number> => {
+    return await ethers.provider.getBlockNumber();
+};
+
+export const mine = async (count = 1): Promise<void> => {
+    expect(count).to.be.gt(0);
+    for (let i = 0; i < count; i++) {
+        await ethers.provider.send("evm_mine", []);
+    }
+};
