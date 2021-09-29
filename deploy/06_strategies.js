fdc005ee98b18058736e8155d916ec9037b45537
Team LevX <team@levx.io>
2021-09-30 08:03:23 +0900

    Update deploy scripts

--- deploy/06_strategies.js
+++ deploy/06_strategies.js
@@ -5,5 +5,4 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     await deploy("FixedPriceSale", { from: deployer, log: true });
     await deploy("EnglishAuction", { from: deployer, log: true });
     await deploy("DutchAuction", { from: deployer, log: true });
-    await deploy("DesignatedSale", { from: deployer, log: true });
 };
