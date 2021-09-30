d75358bcdfe1461d565dfd642df0a0b8d6ddef8a
Team LevX <team@levx.io>
2021-09-30 10:52:17 +0900

    Deploy to mainnet

--- deploy/03_TokenFactory.js
+++ deploy/03_TokenFactory.js
@@ -1,12 +1,15 @@
+const xSUSHI = "0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272";
+
 module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
     const { deployer, admin } = await getNamedAccounts();
     const { deploy, execute, get } = deployments;
 
     const chainId = await getChainId();
+    const recipient = chainId === "1" ? xSUSHI : admin;
     const baseURI = "https://api.shoyunft.com/metadata/" + chainId + "/";
     const result = await deploy("TokenFactory", {
         from: deployer,
-        args: [admin, 25, admin, 0, baseURI, baseURI],
+        args: [recipient, 25, admin, 0, baseURI, baseURI],
         log: true,
     });
 
