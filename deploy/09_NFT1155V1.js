b8821144532766cc9d29a97d9159bfc617584138
Team LevX <team@levx.io>
2021-10-16 09:57:23 +0900

    Add deploy scripts for NFT721V1 and NFT1155V1

---
+++ deploy/09_NFT1155V1.js
@@ -0,0 +1,13 @@
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deployer } = await getNamedAccounts();
+    const { deploy, execute } = deployments;
+
+    const result = await deploy("NFT1155V1", {
+        from: deployer,
+        args: [],
+        log: true,
+    });
+    if (result.newlyDeployed) {
+        await execute("TokenFactory", { from: deployer, log: true }, "upgradeNFT1155", result.address);
+    }
+};
