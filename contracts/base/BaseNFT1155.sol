2bbe10891f12651f5a7ac5c11e3124142f73c406
Team LevX <team@levx.io>
2021-10-20 01:05:54 +0900

    Fix

--- contracts/base/BaseNFT1155.sol
+++ contracts/base/BaseNFT1155.sol
@@ -84,7 +84,7 @@ abstract contract BaseNFT1155 is ERC1155Initializable, OwnableInitializable, IBa
             } else {
                 baseURI = ITokenFactory(_factory).baseURI1155();
                 string memory addy = Strings.toHexString(uint160(address(this)), 20);
-                return string(abi.encodePacked(baseURI, addy, "/", Strings.toString(tokenId), ".json"));
+                return string(abi.encodePacked(baseURI, addy, "/", Strings.toString(id), ".json"));
             }
         }
     }
